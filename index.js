dc632bf089c0eeefd0f5f61a091a409c181c303c
Ashley <alawrence22@ivytech.edu>
2026-02-01 17:32:53 -0500

    Module 3: Tutorial - Node.js and NPM Intro assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+import * as fs from 'fs'
+// import * as cowsay from "cowsay"
+import { say } from "cowsay";
+
+console.log("My name is Ashley!");
+
+const output = say({ text: "mooooo"});
+console.log(output)
+
+// const words = ["study", "test", "coding", "software"];
+// const output = words.join("\n");
+// fs.writeFile("words.txt", output, fileWritten);
+
+// function fileWritten() {
+//     console.log("File has been written.");
+// }
